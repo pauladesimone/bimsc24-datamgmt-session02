a2f90100e61acd062bb15c88399cebd17719c4cc
pauladesimone <paula.de.simone@students.iaac.net>
2024-01-22 22:02:41 -0300

    last changes

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,65 @@
-<!-- // Script is in some way equivalent to script.js. This is place
-to define variables, methods and imports of other Vue compoennts. -->
-<script setup>
-// Understanding ref article: https://blog.logrocket.com/understanding-vue-refs/#:~:text=Ref%20s%20are%20Vue.,element%20in%20your%20Vue%20instance.
-// When ref attribute is added to element, this element then can be referenced
-// in template. It is sort of templatecement of getElementById (but better)
-import { ref } from "vue";
+<!-- App.vue -->
 
-// Define variables and constants
-var count = ref(0);
-
-// Define functions
-function increment() {
-  count.value++;
-}
-</script>
-
-<!-- Template is a HTML-based syntax that allows you to bind the rendered DOM elements
-with data, objects, functions etc. -->
 <template>
-  <div id="top-bar">
-    <div id="title-container">
-      <img class="logo-image" alt="Iaac logo" src="./assets/iaac-white.png" />
-      <h2>Digital Tools for Cloud-based Data Management</h2>
+  <div>
+    <div id="navbar" class="container">
+      <div id="title">Polls attempt of a website</div>
+      <div id="logo">
+        <img src="./assets/cow.jpg" alt="macad cow" />
+      </div>
     </div>
-  </div>
 
-  <div id="content">
-    <!-- First example - button -->
-    <!-- Here we define what function will be called when button is clicked. -->
-    <button @click="increment">Add one more</button>
+    <div id="flex">
+      <div id="sidebar" class="container">
+        <input v-model="inputText" class="definition-input" />
+        <button @click="doSomething">Submit</button>
+        <button @click="refresh">Reset</button>
+        
+        <button @click="updateText">Recite me a poem</button>
+      </div>
+      <div id="main" class="container">
+        <p>{{ text }}</p>
+        <p v-for="paragraph in paragraphs" :key="paragraph.id">{{ paragraph.text }}</p>
 
-    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
-    <p style="margin-left: 8px">Count is: {{ count }}</p>
-
+      </div>
+    </div>
   </div>
 </template>
 
-<!-- Style is for CSS styling -->
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script setup>
+import { ref } from "vue";
 
-  color: #2c3e50;
+const inputText = ref("");
+const paragraphs = ref([]);
+const text = ref("Initial text");
+
+// Array of coding poems with titles
+const codingPoems = [
+  { title: "The Coder's Dream", text: "Roses are red, violets are blue, unexpected '}' on line 32." },
+  { title: "Code Poet", text: "I'm not a coder, I'm a code poet." },
+  { title: "Debugging Blues", text: "Debugging is like being the detective in a crime movie where you are also the murderer." },
+  { title: "The Code Symphony", text: "In the silence of the night, the code symphony plays its melody." },
+  { title: "Ode to the Compiler", text: "Oh, compiler, with your discerning eye, turn my code errors into butterflies." },
+];
+
+function doSomething() {
+  const newParagraph = {
+    id: Date.now(),
+    text: inputText.value,
+  };
+  paragraphs.value.push(newParagraph);
+  inputText.value = "";
 }
 
-#top-bar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: rgba(0, 0, 0, 0.7);
+function refresh() {
+  paragraphs.value = [];
 }
 
-#title-container {
-  display: flex;
-  align-items: center;
-  color: white;
-  margin-right: 1.5rem;
+function updateText() {
+  // Randomly select a coding poem
+  const randomPoemIndex = Math.floor(Math.random() * codingPoems.length);
+  text.value = `${codingPoems[randomPoemIndex].title}: ${codingPoems[randomPoemIndex].text}`;
 }
+</script>
 
-#content {
-  display: flex;
-  padding: 20px
-}
-
-.logo-image {
-  height: 3.25rem;
-  padding: 0.5rem;
-}
-
-h2 {
-  font-size: 1.125rem;
-  font-weight: 600;
-  letter-spacing: -0.05em;
-  font-size: 1.125rem;
-  font-weight: 600;
-  letter-spacing: 0.01em;
-}
-</style>
-
+<style src="./styles/website-styles.css" />
